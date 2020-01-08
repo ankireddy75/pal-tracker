6408c6156780011a947c2ccc18abeb1f9fc0245e
Bill Kable <bkable@pivotal.io>
2020-01-08 11:09:15 -0500

    time controller

--- src/main/java/io/piotal/pal/traker/PalTrackerApplication.java
+++ src/main/java/io/piotal/pal/traker/PalTrackerApplication.java
@@ -2,10 +2,17 @@ package io.piotal.pal.traker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PalTrackerApplication {
     public static void main(String[] args){
         SpringApplication.run(PalTrackerApplication.class, args);
     }
+
+    @Bean
+    public TimeEntryRepository getTimeEntryRepository(){
+        return new InMemoryTimeEntryRepository();
+    }
+
 }
