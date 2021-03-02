800382761fc0fb625853cce51bd6bc33493c6199
ankireddy <ankireddyn@yahoo.com>
2021-03-02 16:16:03 +0000

    tests added

--- src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
@@ -3,7 +3,7 @@ package test.pivotal.pal.tracker;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
-import io.pivotal.pal.tracker.EnvController;
+import io.piotal.pal.traker.EnvController;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
--- src/test/java/test/pivotal/pal/trackerapi/WelcomeApiTest.java
+++ src/test/java/test/pivotal/pal/trackerapi/WelcomeApiTest.java
@@ -1,6 +1,6 @@
 package test.pivotal.pal.trackerapi;
 
-import io.pivotal.pal.tracker.PalTrackerApplication;
+import io.piotal.pal.traker.PalTrackerApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
